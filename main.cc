1dce9ec95bd6301a249114c012b81fcbe69e2734
alexkharl <sasha_xarlamov@mail.ru>
2022-11-20 13:59:39 +0300

    Fix: proper "\n" append

--- main.cc
+++ main.cc
@@ -17,7 +17,6 @@ void exit() { std::exit(EXIT_SUCCESS); }
 int main() {
   std::vector<std::string> entered_input;
   std::vector<std::string> command_history;
-  command_history = {"dgdg dgd", "hell gdg", "hdfdf gdg"};
 
   std::function<void(void)> check_command_history_emptiness{
       [&command_history = std::as_const(command_history)]() {
@@ -72,7 +71,7 @@ int main() {
 
       command_to_execute = (fplus::is_empty(command_history))
                            ? ""
-                           : fplus::append(fplus::last(command_history), std::string{"\n"});
+                           : fplus::last(command_history);
     } else if (fplus::fwd::apply(input_split, fplus::fwd::head(),
                                  fplus::fwd::get_segment(0, 1),
                                  fplus::fwd::is_equal(std::string{"!"}))) {
@@ -89,11 +88,13 @@ int main() {
                 1, fplus::size_of_cont(fplus::head(input_split))),
             fplus::fwd::read_value_with_default<unsigned long long>(-1),
             fplus::subtract(1),
-            fplus::fwd::flip::elem_at_idx(command_history), fplus::fwd::flip::append(std::string{"\n"}));
+            fplus::fwd::flip::elem_at_idx(command_history));
       }
     } else {
-      command_to_execute = fplus::join(std::string{' '}, fplus::append(input_split, std::vector<std::string>{"\n"}));
+      command_to_execute = fplus::join(std::string{' '}, input_split);
     }
-    std::cout << command_to_execute;
+    std::cout << (fplus::size_of_cont(command_to_execute) ? fplus::append(command_to_execute, std::string{"\n"})
+                                                          : command_to_execute);
+    command_history = fplus::append(command_history, std::vector<std::string>{command_to_execute});
   }
 }
